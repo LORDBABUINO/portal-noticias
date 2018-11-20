fd231805d028ea945355724f48257b1bdd49c885
LORDBABUINO <jorge.x7@gmail.com>
2018-11-20 09:29:19 +0000

    - Project arquiteture refactor - Fix server autorefresh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM node
 EXPOSE 3000 9229
 WORKDIR /home/app
-COPY . .
+COPY ./web .
+RUN npm install -g nodemon typescript --save-dev
 RUN npm install
+RUN npm cache clean --force
 CMD npm run dev
---
+++ web/app/app.ts
@@ -0,0 +1,20 @@
+let app = require('express')()
+
+app.set('view engine', 'ejs')
+app.set('views','app/views');
+
+app.get('/', (req: any, res: any) => {
+	res.render('home/index')
+})
+
+app.get('/formulario_inclusao_noticia', (req: any, res: any) => {
+	res.render('admin/form_add_noticia')
+})
+
+app.get('/noticias', (req: any, res: any) => {
+	res.render('noticias/noticias')
+})
+
+app.listen(3000, () => {
+	console.log('Servidor rodando com Expres:anys')
+})
