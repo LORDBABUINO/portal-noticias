f986dc5c38bf0b56bb75178114aa6728c7e5130f
LORDBABUINO <jorge.x7@gmail.com>
2019-03-09 03:05:36 +0000

    Changes table's name

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,14 +1,14 @@
-CREATE TABLE notices(
-	id_notice int not null primary key auto_increment,
+CREATE TABLE news(
+	id_news int not null primary key auto_increment,
 	title varchar(100),
 	content text,
 	creation_date timestamp default current_timestamp
 );
 
-INSERT INTO notices
+INSERT INTO news
 (title, content)
 VALUES('titulo da notícia', 'conteúdo da notícia');
 
-INSERT INTO notices
+INSERT INTO news
 (title, content)
 VALUES('titulo da notícia', 'conteúdo da notícia');
