a8efb26bdad82b0af79d24a57a64c91640e6350e
LORDBABUINO <jorge.x7@gmail.com>
2019-03-11 05:44:24 +0000

    Connection refactoring

--- node/src/config/MysqlConnection.js
+++ node/src/config/MysqlConnection.js
@@ -1,10 +1,15 @@
 import mysql from 'mysql'
-export const MysqlConnection = () => {
-	return mysql.createConnection({
-		host: process.env.MYSQL_HOST,
-		user: process.env.MYSQL_USER,
-		port: process.env.MYSQL_PORT,
-		password: process.env.MYSQL_PASSWORD,
-		database: process.env.MYSQL_DATABASE
-	})
+import util from 'util'
+export class MysqlConnection {
+	constructor(){
+		this._connection = mysql.createConnection({
+			host: process.env.MYSQL_HOST,
+			user: process.env.MYSQL_USER,
+			port: process.env.MYSQL_PORT,
+			password: process.env.MYSQL_PASSWORD,
+			database: process.env.MYSQL_DATABASE
+		})
+	}
+
+	this.connect		 = util.promisify(this._connection.connect.bind(connection))
 }
