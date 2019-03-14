bf15fa2925f6a02364ddbf2e8e1d483704c8712b
LORDBABUINO <jorge.x7@gmail.com>
2019-03-14 04:39:38 +0000

    Refactoring news.js

--- node/src/app/routes/news.js
+++ node/src/app/routes/news.js
@@ -1,20 +1,16 @@
 import { MysqlConnection } from '../../config/MysqlConnection'
 const routeNews = (app) => {
-	const util = require('util')
 
 	app.get('/news', (req, res) => {
 
-		const connection = MysqlConnection()
-		const connect		 = util.promisify(connection.connect.bind(connection))
-		const query			 = util.promisify(connection.query.bind(connection))
-		const end				 = util.promisify(connection.end.bind(connection))
+		const connection = new MysqlConnection()
 
 		const getNoticiasFromDatabase = async () => {
 			try {
-				await connect()
-				const notices = await query('select * from notices')
+				await connection.connect()
+				const notices = await connection.query('select * from notices')
 				res.render('news/news', {notices: notices})
-				await end()
+				await connection.end()
 			} catch(error){
 				console.error(`${error.name}: ${error.message}`)
 			}
