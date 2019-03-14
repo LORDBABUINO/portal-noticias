ce7c14f22c09d28a58d573227bcf096064a57e85
LORDBABUINO <jorge.x7@gmail.com>
2019-03-14 04:47:23 +0000

    Refactoring MysqlConnection.js

--- node/src/config/MysqlConnection.js
+++ node/src/config/MysqlConnection.js
@@ -1,8 +1,10 @@
-import mysql from 'mysql'
-import util from 'util'
-export class MysqlConnection {
+import { createConnection } from 'mysql'
+import { IConnection } from './database/iconnection'
+import { promisify } from 'util'
+export class MysqlConnection extends IConnection {
 	constructor(){
-		this._connection = mysql.createConnection({
+		super()
+		this._connection = createConnection({
 			host: process.env.MYSQL_HOST,
 			user: process.env.MYSQL_USER,
 			port: process.env.MYSQL_PORT,
@@ -10,8 +12,8 @@ export class MysqlConnection {
 			database: process.env.MYSQL_DATABASE
 		})
 
-		this.connect	= util.promisify(this._connection.connect.bind(this._connection))
-		this.query		= util.promisify(this._connection.query.bind(this._connection))
-		this.end			= util.promisify(this._connection.end.bind(this._connection))
+		this.connect	= promisify(this._connection.connect.bind(this._connection))
+		this.query		= promisify(this._connection.query.bind(this._connection))
+		this.end			= promisify(this._connection.end.bind(this._connection))
 	}
 }
---
+++ node/src/config/database/iconnection.js
@@ -0,0 +1,14 @@
+export class IConnection {
+	connect(){
+		throw new Error('Not Implemented Exception')
+	}
+
+	query(sql){
+		throw new Error('Not Implemented Exception')
+	}
+
+	end(){
+		throw new Error('Not Implemented Exception')
+	}
+}
+/**/
